2c147dba961ea874acd37a09b0423d5cd815a271
derdilla <derdilla06@gmail.com>
2023-08-21 19:06:55 +0200

    add BloodPressureRecord tests

--- test/model/bood_pressure_test.dart
+++ test/model/bood_pressure_test.dart
@@ -6,15 +6,18 @@ import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 void main() {
   group('BloodPressureRecord', () {
     test('should initialize with all values supported by dart', () {
-      BloodPressureRecord record = BloodPressureRecord(DateTime.fromMicrosecondsSinceEpoch(0), 0, -50, 1000,
+      BloodPressureRecord record = BloodPressureRecord(DateTime.fromMicrosecondsSinceEpoch(1582991592), 0, -50, 1000,
           "((V⍳V)=⍳⍴V)/V←,V    ⌷←⍳→⍴∆∇⊃‾⍎⍕⌈๏ แผ่นดินฮั่นเABCDEFGHIJKLMNOPQRSTUVWXYZ /0123456789abcdefghijklmnopqrstuvwxyz £©µÀÆÖÞßéöÿ–—‘“”„†•…‰™œŠŸž€ ΑΒΓΔΩαβγδω АБВГДабвг, \n \t д∀∂∈ℝ∧∪≡∞ ↑↗↨↻⇣ ┐┼╔╘░►☺♀ ﬁ�⑀₂ἠḂӥẄɐː⍎אԱა");
 
-      expect(record.creationTime, DateTime.fromMicrosecondsSinceEpoch(0));
+      expect(record.creationTime, DateTime.fromMicrosecondsSinceEpoch(1582991592));
       expect(record.systolic, 0);
       expect(record.diastolic, -50);
       expect(record.pulse, 1000);
       expect(record.notes,
           "((V⍳V)=⍳⍴V)/V←,V    ⌷←⍳→⍴∆∇⊃‾⍎⍕⌈๏ แผ่นดินฮั่นเABCDEFGHIJKLMNOPQRSTUVWXYZ /0123456789abcdefghijklmnopqrstuvwxyz £©µÀÆÖÞßéöÿ–—‘“”„†•…‰™œŠŸž€ ΑΒΓΔΩαβγδω АБВГДабвг, \n \t д∀∂∈ℝ∧∪≡∞ ↑↗↨↻⇣ ┐┼╔╘░►☺♀ ﬁ�⑀₂ἠḂӥẄɐː⍎אԱა");
+    });
+    test('should not save times at or before epoch', () {
+      expect(() => BloodPressureRecord(DateTime.fromMillisecondsSinceEpoch(0), 0, 0, 0, ""), throwsAssertionError);
     });
   });
 
