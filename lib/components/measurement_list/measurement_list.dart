578b3eb177ea5b1ec9a71fb77a7f0c5af5c90904
derdilla <derdilla06@gmail.com>
2023-08-30 17:46:30 +0200

    fix list entries blocked by floating home screen buttons

--- lib/components/measurement_list/measurement_list.dart
+++ lib/components/measurement_list/measurement_list.dart
@@ -10,11 +10,22 @@ class MeasurementList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: entries.length,
+      itemCount: entries.length + 2,
       //separatorBuilder: (context, idx) => const Divider(),
-      itemBuilder: (context, idx) => MeasurementListRow(
-        record: entries[idx]
-      ),
+      itemBuilder: (context, idx) {
+        if (idx == 0) { // first row
+          // TODO
+          return Text("TODO");
+        }
+        if (idx > entries.length) { // last row
+          // Fix actions blocked by floating buttons
+          // This way of doing it seems to be the most common: https://stackoverflow.com/q/29362284
+          return const SizedBox(height: 300,);
+        }
+        return MeasurementListRow(
+            record: entries[idx-1]
+        );
+      },
     );
   }
 
