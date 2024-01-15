c3b52bee952593451fb724da95ea7c2853000e26
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-01-15 16:42:01 +0100

    document useful websites
    Signed-off-by: derdilla <82763757+NobodyForNothing@users.noreply.github.com>

--- lib/model/iso_lang_names.dart
+++ lib/model/iso_lang_names.dart
@@ -16,6 +16,9 @@ String? getDisplayLanguage(Locale l) => switch(l.toLanguageTag()) {
   'tr' => 'Türkçe',
   'zh' => '中文 (简体)',
   'zh-Hant' => '中文（繁體)',
+  // Websites with names for expanding when new languages get added:
+  // - https://chronoplexsoftware.com/localisation/help/languagecodes.htm
+  // - https://localizely.com/locale-code/zh-Hans/
   (_) => (){
     assert(false, l.toString());
     return l.languageCode;
