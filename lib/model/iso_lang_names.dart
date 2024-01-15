f4a55f8da748b2ed4028df80199174a58cae1da8
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-01-15 17:11:52 +0100

    test getDisplayLanguage
    Signed-off-by: derdilla <82763757+NobodyForNothing@users.noreply.github.com>

--- lib/model/iso_lang_names.dart
+++ lib/model/iso_lang_names.dart
@@ -3,24 +3,30 @@ import 'package:flutter/material.dart';
 /// Selects the correct language name for a specific language code.
 ///
 /// Does not account for dialects.
-String? getDisplayLanguage(Locale l) => switch(l.toLanguageTag()) {
+String getDisplayLanguage(Locale l) => switch(l.toLanguageTag()) {
   'en' => 'English',
   'en-US' => 'English (US)',
   'de' => 'Deutsch',
-  'fr' => 'française',
+  'fr' => 'Française',
   'it' => 'Italiano',
   'nb' => 'Norsk bokmål',
   'pt' => 'Português',
-  'pt-BR' => 'português (Brasil)',
-  'ru' => 'русский',
+  'pt-BR' => 'Português (Brasil)',
+  'ru' => 'Русский',
   'tr' => 'Türkçe',
   'zh' => '中文 (简体)',
   'zh-Hant' => '中文（繁體)',
   // Websites with names for expanding when new languages get added:
   // - https://chronoplexsoftware.com/localisation/help/languagecodes.htm
   // - https://localizely.com/locale-code/zh-Hans/
+  //
+  // Names should be modified so they start with an upper case letter.
+  //
   (_) => (){
-    assert(false, l.toString());
+    debugPrint('Unlocalized language tag: $l');
+    debugPrintStack();
+    assert(false, 'Should only be called for localized strings and all '
+        'localized strings are tested.');
     return l.languageCode;
   }()
 };
