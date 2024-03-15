43d90430eaf824984a4808334910f4c962844aef
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-03-15 17:24:46 +0100

    test more
    Signed-off-by: derdilla <82763757+NobodyForNothing@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:blood_pressure_app/model/storage/db/config_db.dart';
 import 'package:blood_pressure_app/model/storage/intervall_store.dart';
 import 'package:blood_pressure_app/model/storage/settings_store.dart';
 import 'package:blood_pressure_app/model/storage/update_legacy_settings.dart';
-import 'package:blood_pressure_app/screens/home_screen.dart';
 import 'package:blood_pressure_app/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -18,6 +17,12 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path/path.dart';
 import 'package:provider/provider.dart';
 import 'package:sqflite/sqflite.dart';
+
+import 'components/dialoges/import_preview_dialoge.dart';
+import 'model/export_import/csv_converter.dart';
+import 'model/export_import/csv_record_parsing_actor.dart';
+import 'model/storage/export_columns_store.dart';
+import 'model/storage/export_csv_settings_store.dart';
 
 late final ConfigDB _database;
 late final BloodPressureModel _bloodPressureModel;
@@ -124,8 +129,19 @@ class AppRoot extends StatelessWidget {
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       locale: settings.language,
-      home: const AppHome(),
-    ),
+      home: ImportPreviewDialoge(
+        bottomAppBar: false,
+        initialActor: CsvRecordParsingActor(
+          CsvConverter(
+            CsvExportSettings(),
+            ExportColumnsManager(),
+          ),
+          'systolic,diastolic,pulse,notes,needlePin\n123,45,67,note1,'
+              '"{""color"":4285132974}"\n114,71,56,,null',
+        ),
+        columnsManager: ExportColumnsManager(),
+      ),
+    )
   );
 
   ThemeData _buildTheme(ColorScheme colorScheme) {
