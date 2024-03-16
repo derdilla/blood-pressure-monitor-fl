90c933553b9db105e798d91b303bec746934e654
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-03-16 18:04:57 +0100

    simplify architecture
    Signed-off-by: derdilla <82763757+NobodyForNothing@users.noreply.github.com>

--- health_data_store/lib/src/repositories/repository.dart
+++ health_data_store/lib/src/repositories/repository.dart
@@ -1,11 +1,11 @@
 import 'dart:collection';
 
-import 'package:health_data_store/src/data_providers/data_provider.dart';
+import 'package:health_data_store/src/database_manager.dart';
 import 'package:health_data_store/src/types/date_range.dart';
 
-/// A repository is a abstraction around multiple [DataProvider]s.
+/// A repository is a abstraction around the [DatabaseManager]
 ///
-/// Repositories wrap the primitive values of [DataProvider]s into more complex
+/// Repositories wrap the primitive values of DB fields into more complex
 /// types and provides domain models for the application.
 abstract class Repository<T> {
   /// Adds a new value to the repository.
