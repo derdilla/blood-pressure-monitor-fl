d3f9dc3fe20523a716e35d2f9a9ea1868d0bd15e
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-03-25 22:45:28 +0100

    implement prefered pressure unit setting
    Signed-off-by: derdilla <82763757+NobodyForNothing@users.noreply.github.com>

--- app/lib/model/blood_pressure/pressure_unit.dart
+++ app/lib/model/blood_pressure/pressure_unit.dart
@@ -6,5 +6,21 @@ enum PressureUnit {
   /// Millimeters of mercury.
   mmHg,
   /// Kilo Pascal
-  kPa,
+  kPa;
+
+  /// Encodes the pressure unit to [decode]able value.
+  int encode() => switch(this) {
+    PressureUnit.mmHg => 0,
+    PressureUnit.kPa => 1,
+  };
+  /// Decodes a pressure unit from an [encode]d value.
+  static PressureUnit? decode(int? encoded) => switch(encoded) {
+    0 => PressureUnit.mmHg,
+    1 => PressureUnit.kPa,
+    null => null,
+    _ => (){
+      assert(false);
+      return null;
+    }(),
+  };
 }
