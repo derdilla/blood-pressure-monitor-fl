de80f9e141ed34433172eb5a2fc74bd6b2a4a994
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-03-25 22:37:04 +0100

    implement pressure unit
    Signed-off-by: derdilla <82763757+NobodyForNothing@users.noreply.github.com>

---
+++ app/lib/model/blood_pressure/pressure_unit.dart
@@ -0,0 +1,10 @@
+
+/// A unit blood pressure can be in.
+///
+/// While mmHg is more common, some devices use kPa
+enum PressureUnit {
+  /// Millimeters of mercury.
+  mmHg,
+  /// Kilo Pascal
+  kPa,
+}
