9c001c7ac382d9d0f6c67d962ae178b4d2bf0f6f
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-03-25 22:45:28 +0100

    implement prefered pressure unit setting
    Signed-off-by: derdilla <82763757+NobodyForNothing@users.noreply.github.com>

--- app/lib/model/storage/settings_store.dart
+++ app/lib/model/storage/settings_store.dart
@@ -2,6 +2,7 @@ import 'dart:collection';
 import 'dart:convert';
 
 import 'package:blood_pressure_app/model/blood_pressure/medicine/medicine.dart';
+import 'package:blood_pressure_app/model/blood_pressure/pressure_unit.dart';
 import 'package:blood_pressure_app/model/horizontal_graph_line.dart';
 import 'package:blood_pressure_app/model/storage/convert_util.dart';
 import 'package:flutter/material.dart';
@@ -43,6 +44,7 @@ class Settings extends ChangeNotifier {
     bool? bottomAppBars,
     List<Medicine>? medications,
     int? highestMedIndex,
+    PressureUnit? preferredPressureUnit,
   }) {
     if (accentColor != null) _accentColor = accentColor;
     if (sysColor != null) _sysColor = sysColor;
@@ -67,6 +69,7 @@ class Settings extends ChangeNotifier {
     if (bottomAppBars != null) _bottomAppBars = bottomAppBars;
     if (medications != null) _medications.addAll(medications);
     if (highestMedIndex != null) _highestMedIndex = highestMedIndex;
+    if (preferredPressureUnit != null) _preferredPressureUnit = preferredPressureUnit;
     _language = language; // No check here, as null is the default as well.
   }
 
@@ -99,6 +102,7 @@ class Settings extends ChangeNotifier {
       medications: ConvertUtil.parseList<String>(map['medications'])?.map((e) =>
           Medicine.fromJson(jsonDecode(e)),).toList(),
       highestMedIndex: ConvertUtil.parseInt(map['highestMedIndex']),
+      preferredPressureUnit: PressureUnit.decode(ConvertUtil.parseInt(map['preferredPressureUnit'])),
     );
 
     // update
@@ -143,6 +147,7 @@ class Settings extends ChangeNotifier {
       'bottomAppBars': bottomAppBars,
       'medications': medications.map(jsonEncode).toList(),
       'highestMedIndex': highestMedIndex,
+      'preferredPressureUnit': preferredPressureUnit,
     };
 
   /// Serialize the object to a restoreable string.
@@ -337,6 +342,14 @@ class Settings extends ChangeNotifier {
   bool get bottomAppBars => _bottomAppBars;
   set bottomAppBars(bool value) {
     _bottomAppBars = value;
+    notifyListeners();
+  }
+
+  PressureUnit _preferredPressureUnit = PressureUnit.mmHg;
+  /// Preferred unit to display and enter measurements in.
+  PressureUnit get preferredPressureUnit => _preferredPressureUnit;
+  set preferredPressureUnit(PressureUnit value) {
+    _preferredPressureUnit = value;
     notifyListeners();
   }
   
