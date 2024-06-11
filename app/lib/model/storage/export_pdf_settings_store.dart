e6d3e34e116cda8e9b2b43dbda06f20bbaa646af
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-06-11 21:34:06 +0200

    add reset to settings classes
    Signed-off-by: derdilla <82763757+NobodyForNothing@users.noreply.github.com>

--- app/lib/model/storage/export_pdf_settings_store.dart
+++ app/lib/model/storage/export_pdf_settings_store.dart
@@ -63,6 +63,19 @@ class PdfExportSettings extends ChangeNotifier implements CustomFieldsSettings {
 
   String toJson() => jsonEncode(toMap());
 
+  /// Reset all fields to their default values.
+  void reset() {
+    final d = PdfExportSettings();
+    _exportTitle = d._exportTitle;
+    _exportStatistics = d._exportStatistics;
+    _exportData = d._exportData;
+    _headerHeight = d._headerHeight;
+    _cellHeight = d._cellHeight;
+    _headerFontSize = d._headerFontSize;
+    _cellFontSize = d._cellFontSize;
+    _exportFieldsConfiguration = d._exportFieldsConfiguration;
+  }
+
   bool _exportTitle = true;
   bool get exportTitle => _exportTitle;
   set exportTitle(bool value) {
