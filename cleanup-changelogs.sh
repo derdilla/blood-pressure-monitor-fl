4bb081adc87b6ad3bf01dc7bacc528673f1711e9
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-07-11 10:05:46 +0900

    Add script for cleaning up changelogs and clean up changelogs

---
+++ cleanup-changelogs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+	echo "Please provide the current app version (e.g 36)."
+  exit 1
+fi
+
+export END=$1-3
+
+for ((i = 0; i <= $END; i++)); do
+  for file in fastlane/metadata/android/*/changelogs/$i.txt; do
+    # Check if the file exists before attempting to delete it
+    if [ -e "$file" ]; then
+      echo "- $file"
+      rm "$file"
+    fi
+  done
+done
