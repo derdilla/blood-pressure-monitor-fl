87e2d4ba8646cd4bebfa15152b19285b99f111d5
derdilla <82763757+NobodyForNothing@users.noreply.github.com>
2024-08-11 13:55:23 +0200

    Update store screenshots
    Signed-off-by: derdilla <82763757+NobodyForNothing@users.noreply.github.com>

--- update-screenshots.sh
+++ update-screenshots.sh
@@ -3,10 +3,10 @@
 OUT_DIR="fastlane/metadata/android/en-US/images/phoneScreenshots"
 
 cd app || exit 1
-flutter drive --target=integration_test/screenshot_home.dart --dart-define=testing_mode=true --driver=test_driver/integration_test.dart  --browser-name android-chrome --android-emulator --flavor github || exit 1
-flutter drive --target=integration_test/screenshot_input.dart --dart-define=testing_mode=true --driver=test_driver/integration_test.dart  --browser-name android-chrome --android-emulator --flavor github || exit 1
-flutter drive --target=integration_test/screenshot_settings.dart --dart-define=testing_mode=true --driver=test_driver/integration_test.dart  --browser-name android-chrome --android-emulator --flavor github || exit 1
-flutter drive --target=integration_test/screenshot_stats.dart --dart-define=testing_mode=true --driver=test_driver/integration_test.dart  --browser-name android-chrome --android-emulator --flavor github || exit 1
+flutter drive --target=integration_test/screenshot_home.dart --dart-define=testing_mode=true --driver=test_driver/integration_test.dart  --browser-name android-chrome --android-emulator --flavor github --no-cache-startup-profile --no-enable-dart-profiling --no-track-widget-creation || exit 1
+flutter drive --target=integration_test/screenshot_input.dart --dart-define=testing_mode=true --driver=test_driver/integration_test.dart  --browser-name android-chrome --android-emulator --flavor github --no-cache-startup-profile --no-enable-dart-profiling --no-track-widget-creation --no-pub || exit 1
+flutter drive --target=integration_test/screenshot_settings.dart --dart-define=testing_mode=true --driver=test_driver/integration_test.dart  --browser-name android-chrome --android-emulator --flavor github --no-cache-startup-profile --no-enable-dart-profiling --no-track-widget-creation --no-pub || exit 1
+flutter drive --target=integration_test/screenshot_stats.dart --dart-define=testing_mode=true --driver=test_driver/integration_test.dart  --browser-name android-chrome --android-emulator --flavor github --no-cache-startup-profile --no-enable-dart-profiling --no-track-widget-creation --no-pub || exit 1
 
 cd build/screenshots
 # remove top 80 px and resize to 2:1 ratio
