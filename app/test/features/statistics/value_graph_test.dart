8470492b348bb6c16bacf9337cd7d9cd3cec5ad3
derdilla <82763757+derdilla@users.noreply.github.com>
2024-11-03 21:11:17 +0100

    Fix graph relies on pulse (#465)
    * require only one graph line to start drawing
    
    * test one category is enough to start drawing

--- app/test/features/statistics/value_graph_test.dart
+++ app/test/features/statistics/value_graph_test.dart
@@ -102,6 +102,31 @@ void main() {
 
     await expectLater(find.byType(BloodPressureValueGraph), matchesGoldenFile('full_graph-years.png'));
   });
+
+  testWidgets('BloodPressureValueGraph is fine with enough values in sys category', (tester) async {
+    await tester.pumpWidget(_buildGraph([
+      mockRecord(time: DateTime(2005), sys: 123),
+      mockRecord(time: DateTime(2003), sys: 110),
+    ], [], []));
+    final localizations = await AppLocalizations.delegate.load(const Locale('en'));
+    expect(find.text(localizations.errNotEnoughDataToGraph), findsNothing);
+  });
+  testWidgets('BloodPressureValueGraph is fine with enough values in dia category', (tester) async {
+    await tester.pumpWidget(_buildGraph([
+      mockRecord(time: DateTime(2005), dia: 123),
+      mockRecord(time: DateTime(2003), dia: 110),
+    ], [], []));
+    final localizations = await AppLocalizations.delegate.load(const Locale('en'));
+    expect(find.text(localizations.errNotEnoughDataToGraph), findsNothing);
+  });
+  testWidgets('BloodPressureValueGraph is fine with enough values in pul category', (tester) async {
+    await tester.pumpWidget(_buildGraph([
+      mockRecord(time: DateTime(2005), pul: 123),
+      mockRecord(time: DateTime(2003), pul: 110),
+    ], [], []));
+    final localizations = await AppLocalizations.delegate.load(const Locale('en'));
+    expect(find.text(localizations.errNotEnoughDataToGraph), findsNothing);
+  });
 }
 
 Widget _buildGraph(
