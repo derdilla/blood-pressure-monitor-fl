bfd49e47ba250b26705eac9c74ba59ae35f62568
derdilla <contact@derdilla.com>
2025-01-20 09:46:00 +0100

    Add tamil lang code

--- app/lib/model/iso_lang_names.dart
+++ app/lib/model/iso_lang_names.dart
@@ -19,8 +19,8 @@ String getDisplayLanguage(Locale l) => switch(l.toLanguageTag()) {
   'zh-Hant' => '中文（繁體)',
   'hu' => 'Magyar (Magyarország)',
   'et' => 'Eesti (Eesti)',
-  'nl' => 'Nederlands',
   'cs' => 'Čeština',
+  'ta' => 'தமிழ்',
   // Websites with names for expanding when new languages get added:
   // - https://chronoplexsoftware.com/localisation/help/languagecodes.htm
   // - https://localizely.com/locale-code/zh-Hans/
