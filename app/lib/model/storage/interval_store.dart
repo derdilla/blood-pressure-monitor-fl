5c3e9da036663750ae5b0aae5a88dc189c2fd535
derdilla <82763757+derdilla@users.noreply.github.com>
2026-05-10 21:45:46 +0200

    Fix parsing of invalid settings data (#675)
    Signed-off-by: derdilla <contact@derdilla.com>

--- app/lib/model/storage/interval_store.dart
+++ app/lib/model/storage/interval_store.dart
@@ -12,7 +12,7 @@ class IntervalStorage extends ChangeNotifier {
   factory IntervalStorage.fromMap(Map<String, dynamic> map) => IntervalStorage(
     stepSize: TimeStep.deserialize(map['stepSize']),
     range: ConvertUtil.parseRange(map['start'], map['end']),
-    timeRange: TimeRange.fromJson(map['timeRange'] as Map<String, dynamic>)
+    timeRange: TimeRange.fromJson(map['timeRange'] as Map<String, dynamic>?)
   );
 
   /// Create a instance from a [String] created by [toJson].
