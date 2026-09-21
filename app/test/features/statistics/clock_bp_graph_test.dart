0e9103876d5eb4b7454966d6e62ed9f5c618ff9a
derdilla <contact@derdilla.com>
2026-09-21 14:42:37 +0200

    update tests

--- app/test/features/statistics/clock_bp_graph_test.dart
+++ app/test/features/statistics/clock_bp_graph_test.dart
@@ -4,66 +4,49 @@ import 'package:blood_pressure_app/features/statistics/clock_bp_graph.dart';
 import 'package:blood_pressure_app/model/storage/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
 
 import '../../model/blood_pressure_analyzer_test.dart';
 import '../../util.dart';
 
 void main() {
   testWidgets("doesn't throw when empty" , (tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: ChangeNotifierProvider<Settings>(
-          create: (_) => Settings(),
-          child: const ClockBpGraph(measurements: []),
-        ),
-      ),
-    ));
+    await tester.pumpWidget(materialApp(const ClockBpGraph(measurements: [])));
     expect(tester.takeException(), isNull);
     expect(find.byType(ClockBpGraph), findsOneWidget);
   });
   testWidgets('[gold] renders sample data like expected in light mode', (tester) async {
     final rng = Random(1234);
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: ChangeNotifierProvider<Settings>(
-          create: (_) => Settings(
-            pulColor: Colors.pink,
-          ),
-          child: ClockBpGraph(measurements: [
-            for (int i = 0; i < 50; i++)
-              mockRecord(
-                time: DateTime.fromMillisecondsSinceEpoch(rng.nextInt(1724578014) * 1000),
-                sys: rng.nextInt(60) + 70,
-                dia: rng.nextInt(60) + 40,
-                pul: rng.nextInt(70) + 40,
-              )
-          ],),
-        ),
-      ),
+    await tester.pumpWidget(materialApp(
+      ClockBpGraph(measurements: [
+        for (int i = 0; i < 50; i++)
+          mockRecord(
+            time: DateTime.fromMillisecondsSinceEpoch(rng.nextInt(1724578014) * 1000),
+            sys: rng.nextInt(60) + 70,
+            dia: rng.nextInt(60) + 40,
+            pul: rng.nextInt(70) + 40,
+          )
+      ],),
+      settings: Settings(
+        pulColor: Colors.pink,
+      )
     ));
     await expectLater(find.byType(ClockBpGraph), myMatchesGoldenFile('ClockBpGraph-light.png'));
   }, tags: 'gold');
   testWidgets('[gold] renders sample data like expected in dark mode', (tester) async {
     final rng = Random(1234);
-    await tester.pumpWidget(MaterialApp(
-      theme: ThemeData.dark(useMaterial3: true),
-      home: Scaffold(
-        body: ChangeNotifierProvider<Settings>(
-          create: (_) => Settings(
-            pulColor: Colors.pink,
-          ),
-          child: ClockBpGraph(measurements: [
-            for (int i = 0; i < 50; i++)
-              mockRecord(
-                time: DateTime.fromMillisecondsSinceEpoch(rng.nextInt(1724578014) * 1000),
-                sys: rng.nextInt(60) + 70,
-                dia: rng.nextInt(60) + 40,
-                pul: rng.nextInt(70) + 40,
-              )
-          ],),
-        ),
-      ),
+    await tester.pumpWidget(materialApp(
+      ClockBpGraph(measurements: [
+        for (int i = 0; i < 50; i++)
+          mockRecord(
+            time: DateTime.fromMillisecondsSinceEpoch(rng.nextInt(1724578014) * 1000),
+            sys: rng.nextInt(60) + 70,
+            dia: rng.nextInt(60) + 40,
+            pul: rng.nextInt(70) + 40,
+          )
+      ],),
+      settings: Settings(
+        pulColor: Colors.pink,
+      )
     ));
     await expectLater(find.byType(ClockBpGraph), myMatchesGoldenFile('ClockBpGraph-dark.png'));
   }, tags: 'gold');
